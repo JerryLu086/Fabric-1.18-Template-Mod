34fbac1d3fbfea16c60d5d8d5973c5a286950426
Jerry Lu <twht.xd@gmail.com>
2026-09-04 18:43:17 +0800

    Initial commit

---
+++ src/main/java/com/example/example_mod/ExampleMod.java
@@ -0,0 +1,17 @@
+package com.example.example_mod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleMod implements ModInitializer {
+	public static final String MOD_ID = "example_mod";
+    public static final String MOD_NAME = "Example Mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+	@Override
+	public void onInitialize() {
+		// Hehe boi
+	}
+}
